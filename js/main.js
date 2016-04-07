07ae97377730c1cbfc26f11add143973fbc6af96
apatheiagit <apatheiar@mail.ru>
2016-04-07 23:39:33 +0300

    Top carousel

--- js/main.js
+++ js/main.js
@@ -1,41 +1,45 @@
 $(function(){	
 	getWeather("Limassol");
-  	$('.current-city').click(function(){
-  		$('.city-list').toggle();
-  		$('.icon-opener').toggleClass('icon-opener-open');
-  	});
-  	$('.city-list li').click(function(){
-  		$('.current-city').children('.name').text($(this).text());
-  		getWeather($(this).children('span').attr('data-city'));
-  		$('.city-list').toggle();
-  		$('.icon-opener').toggleClass('icon-opener-open');
-  	});
-    $('.top-menu>ul>li').hover(function(){
-      var width = $(this).children('a').innerWidth();
-      $(this).children('a').addClass('hover');
-      $(this).children('ul').width(width).slideDown();
-    }, function(){
-      $(this).children('a').removeClass('hover');
-      $(this).children('ul').slideUp();
-    });
-    $('.top-menu>ul>li>ul').hover(function(){
-      $(this).parent().children('a').addClass('hover');
-    }, function(){
-      $(this).parent().children('a').removeClass('hover');;
-    });
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.icon-to-top').fadeIn();
-        } else {
-            $('.icon-to-top').fadeOut();
-        }
-    });
-    $('.icon-to-top').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
-    });
+	$('.current-city').click(function(){
+		$('.city-list').toggle();
+		$('.icon-opener').toggleClass('icon-opener-open');
+	});
+	$('.city-list li').click(function(){
+		$('.current-city').children('.name').text($(this).text());
+		getWeather($(this).children('span').attr('data-city'));
+		$('.city-list').toggle();
+		$('.icon-opener').toggleClass('icon-opener-open');
+	});
+  $('.top-menu>ul>li').hover(function(){
+    var width = $(this).children('a').innerWidth();
+    $(this).children('a').addClass('hover');
+    $(this).children('ul').width(width).slideDown();
+  }, function(){
+    $(this).children('a').removeClass('hover');
+    $(this).children('ul').slideUp();
+  });
+  $('.top-menu>ul>li>ul').hover(function(){
+    $(this).parent().children('a').addClass('hover');
+  }, function(){
+    $(this).parent().children('a').removeClass('hover');;
+  });
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 0) {
+          $('.icon-to-top').fadeIn();
+      } else {
+          $('.icon-to-top').fadeOut();
+      }
+  });
+  $('.icon-to-top').click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 400);
+      return false;
+  });
+  $('.articles--index').owlCarousel({
+    items: 1,
+    loop: true,
+  });
 })
 function getWeather(city){		
 	$.ajax({
